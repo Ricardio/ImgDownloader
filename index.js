84c489e0d23107840450d4c1ee22229a9061cc83
Ricardio <rcspersona@gmail.com>
2015-10-01 01:27:47 -0700

    Some code refactoring. New features include being able to specify extra url numbers for link recursion and using cloud scraper for getting passed cloud fare's ddos protection.

--- index.js
+++ index.js
@@ -1,56 +1,41 @@
-var request = require("request");
+"use strict"
+var cloudscraper = require("cloudscraper");
 var fs = require("fs");
-
-String.prototype.regexIndexOf = function(regex, startpos) {
-	var indexOf = this.substring(startpos || 0).search(regex);
-	return (indexOf >= 0) ? (indexOf + (startpos || 0)) : indexOf;
-}
-var lastIndexOf = String.prototype.lastIndexOf;
-var regexLastIndexOf = String.prototype.regexLastIndexOf;
+var websites = require("./websites.json");
+var request = require("request")
 
 var i = 0,  max = 100;
-
-function Website(srcPatt, urlPatt, filters, urlFix, name) {
-	this.srcPatt = srcPatt;
-	this.urlPatt = urlPatt;
-	this.filters = filters;
-	this.urlFix = urlFix;
-	this.name = name;
-}
-eval(fs.readFileSync('websites.js')+'');
-//delete line above and add own websites using example below
-
-/* 	Example
-	var allImgsOnPage = new Website("srcPatt", [], ["s."], "http:", "nameinlink");
-	var recursionPages = new Website("srcPatt", ["/", ""], [], "", "nameinlink");
-	var websites = [allImgOnPage, recursionPages];
-	var link = "http://www://nameinlinkddowddsds.com";
-*/
+var link = "";
 
 var website = checkIn(link, websites); /*debug*/ console.log(website.name);
-var newLink = replaceAt(link, website.urlPatt, "*");
+var web = replaceAt(link, website.urlPatt, "*");
 
-var num1 = isolate(website.urlPatt, link, lastIndexOf);
-request(link, function(error, response, body){
+cloudscraper.get(link, function(error, body, response){
 	htmlParser(body);
 });
 
 function htmlParser(html) {
-	var regex = new RegExp("\"(https?:)?/?/" + website.srcPatt + "(.*?)\"", "g");
-	html.replace(regex, cb);
+	var regex = new RegExp("\"(?:[^\"\\\\]|\\.)*" + website.srcPatt + "(.*?)\"", "g");
+	var newLink = web.link;
+	var src = html;
+	src.replace(regex, cb);
 
-	if(website.urlPatt.length > 0)
-		request(numLink(num1, newLink, ++i), function(error, response, body){
+	if(website.urlPatt.length > 0){
+		for(var j = 0; j < web.numStarts.length; j++)
+			newLink = numLink(web.numStarts[j], newLink, i);
+		i++
+		console.log(newLink);
+		cloudscraper.get(newLink, function(error, body, response){
 			htmlParser(body);
 		});
+	}
 
 	function cb(match) {
 		var url = website.urlFix + match.replace("\"", "").replace("\"","");
-		var fileName = isolate(["/", url.length], url, lastIndexOf);
-
+		var fileName = isolate(["/", url.length], url);
 		if (website.filters.length > 0)
 			if(checkIn(website.filters, "extFix") === "extFix"){
-				var extenFix = isolate(["jpg", fileName.length], fileName, lastIndexOf);
+				var extenFix = isolate(["jpg", fileName.length], fileName);
 				fileName = fileName.replace(extenFix, "");
 			} else if(checkIn(fileName, website.filters))
 				return;
@@ -65,36 +50,59 @@ function htmlParser(html) {
 }
 
 
-function checkIn(str, item){
+function checkIn(arr, item){
 	var check;
-	for(var i = 0; i < str.length; i++){
-		if(typeof str !== "string"){
-			check = str[i].indexOf(item);
+	for(var i = 0; i < arr.length; i++){
+		if(typeof arr !== "string"){
+			check = arr[i].indexOf(item);
 			if(check > -1) return str[i];
 		}
 
 		if(typeof item[i] === "object")
-			check = str.indexOf(item[i].name);
+			check = arr.indexOf(item[i].name);
 		else
-			check = str.indexOf(item[i]);
+			check = arr.indexOf(item[i]);
 		if(check > -1) return item[i];
 	}
 }
 
-function isolate(arr, item, func){
-	var bef = arr[0];
-	var aft = arr[1];
+function isolate(arr, item){
+	var bef = arr[0], aft = arr[1];
 	if(typeof arr[0] !== "number")
-		bef = func.call(item, arr[0]) + arr[0].length;
+		bef = item.lastIndexOf(arr[0]) + arr[0].length;
 	if(typeof arr[1] !== "number")
-		aft = func.call(item, arr[1]);
+		aft = item.lastIndexOf(arr[1]);
 	return item.substring(bef, aft);
 }
 
 function replaceAt(str, arr, char){
-	var bef = str.substring(0, str.lastIndexOf(arr[0]) + arr[0].length)
-	var aft = str.substring(str.lastIndexOf(arr[1]) + char.length);
-	return bef + char + aft;
+	var obj = {
+		numStarts : [],
+		link : str
+	}
+	var befIndex, aftIndex;
+	for(var t = 0; t < arr.length; t++){
+		if(typeof arr[t][0] !== "number"){
+			befIndex = obj.link.lastIndexOf(arr[t][0]) + arr[t][0].length;
+			aftIndex = obj.link.lastIndexOf(arr[t][1]);
+		} else {
+			befIndex = xIndexOf(obj.link, arr[t][1], arr[t][0]) + arr[t][1].length;
+			aftIndex = xIndexOf(obj.link, arr[t][3], arr[t][2]);
+		}
+		obj.numStarts.push(obj.link.substring(befIndex, aftIndex));
+		var befString = obj.link.substring(0, befIndex);
+		var aftString = obj.link.substring(aftIndex);
+		obj.link = befString + char + aftString;
+	}
+	return obj
+}
+
+function xIndexOf(str, val, amt){
+	var temp = str.indexOf(val);
+	if(amt > 1)
+		for (var i = 1; i < amt; i++)
+			temp = str.indexOf(val, temp + 1)
+	return temp;
 }
 
 function numLink(strNum, item, iter){
